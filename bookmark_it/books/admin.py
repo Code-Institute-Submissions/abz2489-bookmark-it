8dd272a7d2c88cf97dcf041e561f84d33beb7637
Abigail Davies <abz2489@gmail.com>
2024-03-29 23:11:27 +0000

    Customise admin panel for book and category

--- bookmark_it/books/admin.py
+++ bookmark_it/books/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
 from .models import Category, Book
 
-admin.site.register(Category)
-admin.site.register(Book)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        "friendly_name",
+        "name"
+    )
+    
+class BookAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "isbn",
+        "title",
+        "author",
+        "price",
+        "category",
+        "cover"
+    )
+
+    ordering = ("id",)
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Book, BookAdmin)
