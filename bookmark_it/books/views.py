95201156bbee67b694b3423ee8f88bbd8fec5ae2
Abigail Davies <abz2489@gmail.com>
2024-03-31 21:58:56 +0000

    Add sorting by title, author and price to all books view

--- bookmark_it/books/views.py
+++ bookmark_it/books/views.py
@@ -1,6 +1,8 @@
+from unicodedata import category
 from django.shortcuts import render,get_object_or_404, redirect, reverse
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
 from .models import Category, Book
 
 
@@ -10,11 +12,31 @@ def all_books(request):
     books = Book.objects.all()
     query = None
     categories = None
+    sort = None
+    direction = None
 
-    """Search & sort"""
+    """Search queries, sorting by title, author, price and category"""
     if request.GET:
+        if "sort" in request.GET:
+            sortkey = request.GET["sort"]
+            sort = sortkey
+            if sortkey == "title":
+                print(f"Sort Key: {sortkey}")
+                sortkey = "lower_title"
+                books = books.annotate(lower_title=Lower("title"))
+
+            if sortkey == "author":
+                sortkey = "lower_author"
+                books = books.annotate(lower_author=Lower("author"))
+
+            if "direction" in request.GET:
+                direction = request.GET["direction"]
+                if direction == "desc":
+                    sortkey = f"-{sortkey}"
+            books = books.order_by(sortkey)
+
         if "category" in request.GET:
-            categories = request.GET['category'].split(",")
+            categories = request.GET["category"].split(",")
             books = books.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
 
@@ -23,17 +45,20 @@ def all_books(request):
             print(f"Query: {query}")
             if not query:
                 messages.error(request, "No search criteria entered!")
-                return redirect(reverse('books'))
+                return redirect(reverse("books"))
             
             queries = Q(title__icontains=query) | Q(author__icontains=query) | Q(summary__icontains=query) | Q(category__name__icontains=query)
             books = books.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
-        'books': books,
-        'search_term': query,
+        "books": books,
+        "search_term": query,
         "current_categories": categories,
+        "current_sorting": current_sorting
     }
-    return render(request, 'books/books.html', context)
+    return render(request, "books/books.html", context)
 
 
 def book_summary(request, book_id):
