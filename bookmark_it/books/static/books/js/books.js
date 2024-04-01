19fed1b546f9f084c1e7e8ea70d1627c1808f8d4
Abigail Davies <abz2489@gmail.com>
2024-04-01 08:31:41 +0000

    Add media match event for mobile devices to show bolder style

--- bookmark_it/books/static/books/js/books.js
+++ bookmark_it/books/static/books/js/books.js
@@ -8,11 +8,11 @@ function optionUrl(option) {
         let url = selectedOption.dataset.url;
         if (url) {
             window.location.href = url;
-            console.log(url)
         }
         return false;
     }
 
+    /*Functions to switch the bootstrap arrow icon depending on hover state*/
     function fillSquare(icon) {
         let arrowUp = document.getElementById("arrow-up");
         arrowUp.classList.remove("bi-arrow-up-square");
@@ -25,6 +25,27 @@ function optionUrl(option) {
         arrowUp.classList.add("bi-arrow-up-square");
     }
 
+    /*Click event that takes the user back to the top of the page*/
     function scrollToTop() {
         window.scrollTo(0,0);
     }
+
+    /*Media query match event to determine mobile style of back to top button*/
+    function mobileFill(mQuery) {
+        let arrowUp = document.getElementById("arrow-up");
+        if (mQuery.matches) {
+            arrowUp.classList.remove("bi-arrow-up-square");
+            arrowUp.classList.add("bi-arrow-up-square-fill");
+        } else {
+            arrowUp.classList.remove("bi-arrow-up-square-fill");
+            arrowUp.classList.add("bi-arrow-up-square");
+        }
+    }
+
+    let mQuery = window.matchMedia("(max-width: 767px)")
+
+    mobileFill(mQuery)
+
+    mQuery.onchange = function() {
+        mobileFill(mQuery);
+      }
