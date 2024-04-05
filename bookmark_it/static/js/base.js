669c17d011c592e6fcdb71897eda7e6bcd5af84c
Abigail Davies <abz2489@gmail.com>
2024-04-05 22:13:16 +0000

    Move js toast triggers to base js

--- bookmark_it/static/js/base.js
+++ bookmark_it/static/js/base.js
@@ -1,2 +1,15 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const getToast = document.getElementById('toast');
+    let toast = new bootstrap.Toast(getToast);
+    toast.show();
+
+    const closeToast = document.getElementById('toast-close');
+    closeToast.addEventListener('click', () => {
+    toast.hide();
+})
+});
+    
