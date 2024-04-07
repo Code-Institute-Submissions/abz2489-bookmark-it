94313afbfd374c67d83b4427bf30c5a7dca71b77
Abigail Davies <abz2489@gmail.com>
2024-04-07 21:46:00 +0000

    Change secret key and debug variables

--- bookmark_it/settings.py
+++ bookmark_it/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xo#l)oqq-ydh8p(m(g!-)3h1t$tuc^$*a84ps+yy38%^40_#+j'
+SECRET_KEY = os.environ.get('SECRET_KEY', ' ')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     '8000-abz2489-bookmarkit-su6afnii4cv.ws-eu110.gitpod.io',
