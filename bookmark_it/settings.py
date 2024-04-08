00e049ca3a97ec1e80949ca28ae20826f8f230ae
Abigail Davies <abz2489@gmail.com>
2024-04-08 20:44:06 +0000

    Switch to local database

--- bookmark_it/settings.py
+++ bookmark_it/settings.py
@@ -133,17 +133,17 @@ WSGI_APPLICATION = 'bookmark_it.wsgi.application'
 #       'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 #     }
 # else:
-# DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
-#       }
-#     }
+DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+      }
+    }
     
 
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-}
+#DATABASES = {
+    #'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+#}
 
 
 # Password validation
