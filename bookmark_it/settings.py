b950c69392c792b0685cee0b06799de4888d7fcc
Abigail Davies <abz2489@gmail.com>
2024-04-08 00:11:01 +0000

    Add s3 bucket config to settings

--- bookmark_it/settings.py
+++ bookmark_it/settings.py
@@ -188,6 +188,14 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+if 'USE_AWS' in os.environ:
+    # Bucket Config
+    AWS_STORAGE_BUCKET_NAME = 'bookmark-it'
+    AWS_S3_REGION_NAME = 'eu-west-2'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
 
 # Stripe
 FREE_DELIVERY = 30
