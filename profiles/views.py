548d3a6d2a73d33c3cf3ea1ed44d58078558c2fe
Abigail Davies <abz2489@gmail.com>
2024-04-09 20:19:21 +0000

    Write post handler for profile view

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 
 from .models import UserProfile
 from .forms import UserProfileForm
@@ -7,6 +8,16 @@ def profile(request):
     """ Display the user's profile. """
 
     profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == "POST":
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Delivery info updated successfully")
+        else:
+            messages.error(request, "Failed to update delivery info. Please check the form is filled out correctly.")
+    else:
+        form = UserProfileForm(instance=profile)
 
     form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
