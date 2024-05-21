98d9b170454da93dba55a7b58fa1b57426f8a5b3
Abigail Davies <abz2489@gmail.com>
2024-05-21 20:46:20 +0000

    Change date published to date field

--- books/models.py
+++ books/models.py
@@ -19,12 +19,12 @@ class Book(models.Model):
     title = models.CharField(max_length=254)
     summary = models.TextField(max_length=500)
     author = models.CharField(max_length=254)
-    isbn = models.BigIntegerField()
+    isbn = models.BigIntegerField(unique=True)
     price = models.DecimalField(max_digits=5, decimal_places=2)
     pages = models.IntegerField(null=True, blank=True)
     series = models.CharField(max_length=254, null=True, blank=True)
     number_in_series = models.IntegerField(null=True, blank=True)
-    date_published = models.CharField(max_length=254, null=True, blank=True)
+    date_published = models.DateField(null=True, blank=True)
     cover = models.ImageField(null=True, blank=True)
 
     def __str__(self):
