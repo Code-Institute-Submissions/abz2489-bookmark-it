c50b71ad42227f259e21e74557c17a55ce76e501
Abigail Davies <abz2489@gmail.com>
2024-05-29 16:37:56 +0000

    Change add book form back to labels and hide boomark field

--- books/forms.py
+++ books/forms.py
@@ -7,7 +7,6 @@ from .models import Category, Book
 class BookForm(forms.ModelForm):
     class Meta:
         model = Book
-        print(model)
         fields = "__all__"
 
     def __init__(self, *args, **kwargs):
@@ -19,41 +18,20 @@ class BookForm(forms.ModelForm):
         categories = Category.objects.all()
         friendly_names = [(category.id, category.get_friendly_name()) for category in categories]
         self.fields["category"].choices = friendly_names
-
-        placeholders = {
-            "category": "",
-            "title": "Title",
-            "summary": "Summary",
-            "author": "Author",
-            "isbn": "ISBN",
-            "price": "Price",
-            "pages": "Pages",
-            "series": "Series",
-            "number_in_series": "Number in Series",
-            "date_published": "Date Published",
-            "cover": "Cover Image"
-        }
         
         for field_name, field in self.fields.items():
-            if field_name in placeholders:
-                if field.required:
-                    placeholder = f"{placeholders[field_name]} *"
-                else:
-                    placeholder = placeholders[field_name]
-            field.widget.attrs["placeholder"] = placeholders[field_name]
-            
-            if field_name != "cover":
-                field.label = ""
-            else:
-                field.label = "Cover"
-
             if field_name == "isbn" or "price":
                 field.widget.attrs["class"] = "remove-spinner"
 
             if field_name == "date_published":
                 field.widget = forms.DateInput(attrs={"type": "date"})
             
+            if field_name == "bookmark":
+                field.widget.attrs["class"] = "hide"
+                field.label = ""
+             
     def clean_date_published(self):
+        """Ensures only past dates are selected in the date picker"""
         date_published = self.cleaned_data.get("date_published")
         if date_published >= datetime.date.today():
             raise forms.ValidationError("The publication date must be in the past!")
