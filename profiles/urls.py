466c70525a1e28bc212edfd886bc0cc808c48abb
Abigail Davies <abz2489@gmail.com>
2024-06-01 22:04:31 +0000

    Add urls for bookmark add and bookmark

--- profiles/urls.py
+++ profiles/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.profile, name='profile'),
     path('order_history/<order_number>', views.order_history, name='order_history'),
-    path('bookmark_add/<int:book_id>/', views.bookmark_add, name='bookmark_add'),
-    path('user_bookmarks/', views.user_bookmarks, name='user_bookmarks'),
+    path('bookmark_add/int:<book_id>/', views.bookmark_add ,name='bookmark_add'),
+    path('bookmark/', views.bookmark, name='bookmark'),
 ]
