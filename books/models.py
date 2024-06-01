6efdf7ba78be52fe32e57914d907a5a3325913b4
Abigail Davies <abz2489@gmail.com>
2024-06-01 21:58:30 +0000

    Remove bookmarks field from books model

--- books/models.py
+++ books/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 
 class Category(models.Model):
@@ -18,17 +17,16 @@ class Category(models.Model):
 
 class Book(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
-    title = models.CharField(max_length=254)
-    summary = models.TextField(max_length=1000)
-    author = models.CharField(max_length=254)
-    isbn = models.BigIntegerField(unique=True)
-    price = models.DecimalField(max_digits=5, decimal_places=2)
+    title = models.CharField(max_length=254, null=False, blank=False)
+    summary = models.TextField(max_length=1000, null=False, blank=False)
+    author = models.CharField(max_length=254, null=False, blank=False)
+    isbn = models.BigIntegerField(unique=True, null=False, blank=False)
+    price = models.DecimalField(max_digits=5, decimal_places=2, null=False, blank=False)
     pages = models.IntegerField(null=True, blank=True)
     series = models.CharField(max_length=254, null=True, blank=True)
     number_in_series = models.IntegerField(null=True, blank=True)
     date_published = models.DateField(null=True, blank=True)
     cover = models.ImageField(null=True, blank=True)
-    bookmarks = models.ManyToManyField(User, related_name='bookmarks', default=None, blank=True)
 
     def __str__(self):
         return self.title
